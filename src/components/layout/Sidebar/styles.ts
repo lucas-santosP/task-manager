dc36c4a0f4c05c9e1419dcb5178a333455eb2e29
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-03-22 16:52:25 -0300

    refactor: Improve variant styles on sidebar

--- src/components/layout/Sidebar/styles.ts
+++ src/components/layout/Sidebar/styles.ts
@@ -1,4 +1,4 @@
-import styled, { DefaultTheme } from "styled-components";
+import styled, { DefaultTheme, css } from "styled-components";
 import { shade } from "polished";
 
 interface IStyledProps {
@@ -31,7 +31,6 @@ export const NavList = styled.ul`
   display: flex;
   height: 100%;
   flex-direction: column;
-  color: ${({ theme }) => theme.colors.secondary};
 `;
 
 export const NavItem = styled.li<INavItem>`
@@ -39,16 +38,20 @@ export const NavItem = styled.li<INavItem>`
   height: 45px;
   margin: 0.5rem 0;
   transition: all ease 0.3s;
-  background-color: ${({ theme, isSelected }) => {
-    if (!isSelected) return "none";
-    return navItemBackground(theme);
-  }};
+  color: ${({ theme }) => theme.colors.secondary};
+  cursor: pointer;
 
   &:hover {
-    cursor: pointer;
-    ${({ isSelected }) => (isSelected ? "cursor:default;" : "")}
     background-color: ${({ theme }) => navItemBackground(theme)};
   }
+
+  ${({ theme, isSelected }) =>
+    isSelected &&
+    css`
+      cursor: default;
+      background-color: none;
+      background-color: ${navItemBackground(theme)};
+    `}
 
   svg,
   img {
@@ -69,6 +72,12 @@ export const NavItemText = styled.span<IStyledProps>`
   font-size: 1.2rem;
   white-space: nowrap;
   transition: all ease 0.3s;
-  transition-delay: ${({ isExpanded }) => (isExpanded ? "200ms" : "0ms")};
-  opacity: ${({ isExpanded }) => (isExpanded ? 1 : 0)};
+  opacity: 0;
+
+  ${({ isExpanded }) =>
+    isExpanded &&
+    css`
+      opacity: 1;
+      transition-delay: 200ms;
+    `}
 `;
