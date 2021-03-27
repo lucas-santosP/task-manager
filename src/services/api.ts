4cb2afb0fc49899b0836bfd83aee2599c64aed49
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-03-27 13:00:07 -0300

    feat: Add setAuthHeader in the api file

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,3 +4,10 @@ export const API = axios.create({
   baseURL: "http://localhost:3333/api",
   headers: { "Content-Type": "application/json" },
 });
+
+export function setAuthHeader(token: string): void {
+  API.interceptors.request.use((req) => {
+    req.headers.Authorization = `Bearer ${token}`;
+    return req;
+  });
+}
