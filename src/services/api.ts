d901da84492ba1b37287621016d07593b993b37f
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-03-28 15:43:36 -0300

    refactor: Using env variable to access the api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:3333/api",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: { "Content-Type": "application/json" },
 });
 
