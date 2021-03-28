1522442ce23dcac10e121efc4f1837cda00eb980
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-03-28 16:39:35 -0300

    feat: Add link component

---
+++ src/components/ui/Link/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+import { Link as WouterLink } from "wouter";
+
+interface IProps {
+  to: string;
+}
+
+const StyledLink = styled(WouterLink)`
+  color: ${({ theme }) => theme.colors.textHighlight};
+`;
+
+const Link: React.FC<IProps> = (props) => {
+  const { children, to } = props;
+
+  return <StyledLink to={to}>{children}</StyledLink>;
+};
+
+export default Link;
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,7 @@
 import Card from "./Card";
 import Button from "./Button";
 import Input from "./Input";
+import Link from "./Link";
 import AppendButtonToggleTheme from "./AppendButtonToggleTheme";
 
-export { Button, Input, Card, AppendButtonToggleTheme };
+export { Button, Input, Link, Card, AppendButtonToggleTheme };
