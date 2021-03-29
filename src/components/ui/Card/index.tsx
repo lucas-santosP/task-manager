3fbb20d8333872a3ef719daf3aeb90b779f12696
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-03-28 23:29:13 -0300

    feat: Add a header in the card component and move styles to it own file

--- src/components/ui/Card/index.tsx
+++ src/components/ui/Card/index.tsx
@@ -1,28 +1,24 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import { CardContainer, CardHeader } from "./styles";
 
-const CardContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  color: ${({ theme }) => theme.colors.text};
-  border-radius: 0.5rem;
-  padding: 2rem;
-  width: 100%;
-  max-width: 500px;
-  box-shadow: 0 2px 5px 5px rgba(0, 0, 0, 0.205);
+interface IProps {
+  title?: string;
+}
 
-  ${({ theme }) =>
-    theme.title === "light"
-      ? css`
-          background-color: #fff;
-        `
-      : css`
-          background-color: ${theme.colors.primary};
-        `}
-`;
+const Card: React.FC<IProps> = (props) => {
+  const { children, title } = props;
 
-const Card: React.FC = ({ children }) => {
-  return <CardContainer>{children}</CardContainer>;
+  return (
+    <CardContainer>
+      {title && (
+        <CardHeader>
+          <h2>{title}</h2>
+        </CardHeader>
+      )}
+
+      <main>{children}</main>
+    </CardContainer>
+  );
 };
 
 export default Card;
---
+++ src/components/ui/Card/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${({ theme }) => theme.colors.text};
+  border-radius: 0.5rem;
+  padding: 2rem;
+  width: 100%;
+  max-width: 500px;
+  box-shadow: 0 2px 5px 5px rgba(0, 0, 0, 0.205);
+  background-color: ${({ theme }) => (theme.title === "light" ? " #fff" : theme.colors.primary)};
+
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const CardHeader = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 1.5rem;
+
+  h2 {
+    font-size: ${({ theme }) => theme.fontSize.md};
+    font-family: ${({ theme }) => theme.fontFamily.secondary};
+  }
+`;
