5201b1124edca7d20c3a6878be993eeac37e9ae0
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-04-09 12:39:32 -0300

    feat: Improve button hover effect

--- src/components/ui/Button/styles.ts
+++ src/components/ui/Button/styles.ts
@@ -1,39 +1,43 @@
+import { lighten } from "polished";
 import styled, { css } from "styled-components";
+import { baseTransition } from "../../../styles/shared";
 
 interface IProps {
   isLoading?: boolean;
 }
 
 export const StyledButton = styled.button<IProps>`
+  ${baseTransition}
   position: relative;
   display: flex;
   align-items: center;
   min-height: 2rem;
   padding: ${({ theme }) => theme.spacing.xs + " " + theme.spacing.lg};
-  border-radius: 9999px;
-  background-color: ${({ theme }) => theme.colors.primary};
   color: ${({ theme }) => theme.colors.secondary};
   font-size: 1.2rem;
   text-align: center;
-
-  &:hover {
-    transition: all ease 0.3s;
-    filter: brightness(120%);
-  }
-
-  &:disabled {
-    cursor: wait;
-    filter: brightness(120%);
-  }
+  border-radius: 9999px;
 
   ${({ theme }) =>
     theme.title === "light"
       ? css`
           background-color: ${theme.colors.primary};
+          &:hover {
+            transition: all ease 0.3s;
+            background-color: ${lighten(0.09, theme.colors.primary)};
+          }
         `
       : css`
           background-color: ${theme.colors.gray};
+          &:hover {
+            background-color: ${lighten(0.09, theme.colors.gray)};
+          }
         `}
+
+  &:disabled {
+    cursor: wait;
+    filter: brightness(120%);
+  }
 
   svg {
     position: absolute;
